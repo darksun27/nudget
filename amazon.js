45e1430dd5ae2f1e0d1bc83cc275c05b695c7f05
darksun27 <sbsiddharth@gmail.com>
2021-03-05 01:48:50 +0530

    Initial Push

---
+++ amazon.js
@@ -0,0 +1,29 @@
+let amazon_priceblock = document.getElementById('price');
+let location_href = document.location.href;
+if(amazon_priceblock) {
+    amazon_priceblock.innerHTML = 
+        "<p>Do You Really Want to Buy this?</p>" + amazon_priceblock.innerHTML;
+    let buy_now = document.getElementById('buyNow');
+    if(buy_now) {
+        buy_now.innerHTML += "<p>Ask yourself if you really need this!</p>";
+        buy_now.addEventListener('click', (e) => {
+            let costprice = document.getElementById('priceblock_ourprice');
+            let cp = "";
+            costprice.innerText.split('').forEach((num) => {
+                if(Number.isInteger(parseInt(num)) || num == '.') {
+                    cp+=num;
+                }
+            })
+            let response = confirm("Do you really want to buy this?");
+            if(response == false) {
+                e.preventDefault();
+                window.location.replace(location_href);
+            } else {
+                chrome.storage.sync.set({'cost':parseInt(cp)});
+                window.chrome.runtime.sendMessage({
+                    action: "add"
+                });
+            }
+        })
+    }
+}
---
+++ popup.js
@@ -0,0 +1,60 @@
+let glimit = 0;
+
+window.chrome.storage.sync.get('progress', (items) => {
+    if(!items.progress) {
+        window.chrome.storage.sync.set({'progress':0});
+    }
+})
+
+window.chrome.storage.sync.get('limit', (items) => {
+    if(!items.limit) {
+
+    } else {
+        glimit = items.limit;
+        document.getElementById('goal').style.display = "block";
+        document.getElementById('setgoal').style.display = "none";
+    }
+})
+
+$(function(){
+    window.chrome.storage.sync.get('progress', (items) => {
+        if(items.progress >= 0) {
+            console.log(items.progress)
+            let progress_bar = document.getElementById('status');
+            progress_bar.value = items.progress;
+            let bar_text = document.getElementById('bartext');
+            bar_text.innerText = (items.progress).toString() + '/' + glimit.toString();
+            let ug = document.getElementById('ug');
+            ug.innerText = glimit.toString();
+        }
+    })
+
+    $('#limitbtn').click(() => {
+        let limit = document.getElementById('limit').value;
+        if(limit > 0) {
+            window.chrome.storage.sync.set({'limit': limit}, () => {
+                document.getElementById('goal').style.display = "block";
+                document.getElementById('setgoal').style.display = "none";
+                location.reload();
+            });
+        }
+    })
+    $('#climitbtn').click(() => {
+        let limit = document.getElementById('climit').value;
+        let reason = document.getElementById('r').value
+        if(limit > 0 && reason) {
+            window.chrome.storage.sync.set({'limit': limit}, () => {
+                document.getElementById('goal').style.display = "block";
+                document.getElementById('setgoal').style.display = "none";
+                document.getElementById('reason').style.display = "none";
+                location.reload();
+            });
+        }
+    })
+
+    $('#cg').click(() => {
+        document.getElementById('goal').style.display = "none";
+        document.getElementById('setgoal').style.display = "none";
+        document.getElementById('reason').style.display = "block";
+    })
+})
