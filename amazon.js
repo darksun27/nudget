5311b3fd69f36fd39cd61342b69fa8b950d73a26
darksun27 <sbsiddharth@gmail.com>
2021-03-08 02:44:24 +0530

    Add popup to appear

--- amazon.js
+++ amazon.js
@@ -1,6 +1,33 @@
 let amazon_priceblock = document.getElementById('price');
 let location_href = document.location.href;
 if(amazon_priceblock) {
+    let popup = document.createElement("DIV");
+    popup.id = "top-nudget";
+    popup.title = "";
+    popup.innerHTML = `
+        <div class="ui verical segment">
+            <h1 class="ui center aligned header">Nudget</h1>
+        </div>
+        <div id="setgoal">
+            <div class="ui secondary segment center aligned vertical">
+            <h3 class="ui center aligned header">Set Maximum Limit</h3>
+            <div class="ui container">
+                This would cost you so much money!!!!
+                <button onclick="func()" class="ui primary button" id="limitbtn">
+                    I am just Exploring
+                </button>
+                <button onclick="cl()">
+                No Close this Tab
+                </button>
+            </div>
+            </div>
+        </div>
+    `;
+    document.body.appendChild(popup);
+    var newScript = document.createElement("script");
+    var inlineScript = document.createTextNode(`const func = () => {document.getElementById('top-nudget').style.visibility="hidden";}`);
+    newScript.appendChild(inlineScript); 
+    document.body.appendChild(newScript);
     amazon_priceblock.innerHTML = 
         "<p>Do You Really Want to Buy this?</p>" + amazon_priceblock.innerHTML;
     let buy_now = document.getElementById('buyNow');
@@ -14,17 +41,17 @@ if(amazon_priceblock) {
                 if(Number.isInteger(parseInt(num)) || num == '.') {
                     cp+=num;
                 }
-            })
+            });
             let response = confirm("Do you really want to buy this?");
             if(response == false) {
                 e.preventDefault();
-                window.location.replace(location_href);
+                popup.style.visibility = 'hidden';
             } else {
                 chrome.storage.sync.set({'cost':parseFloat(cp)*qty});
                 window.chrome.runtime.sendMessage({
                     action: "add"
                 });
-            }
+            }            
         })
     }
 }
